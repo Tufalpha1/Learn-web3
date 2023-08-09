f21dcd49993f8b0480610703a7b2824f7e078f4a
unknown <zaynzafar18@gmail.com>
2023-08-09 17:31:10 +0500

    cryptodevs DAO

---
+++ onchain-dao/frontend/constants.js
@@ -0,0 +1,7 @@
+export const CryptoDevsNFTAddress = "0x457127912929585Cee056Eec642aCFe7f35cDF32";
+export const FakeNFTMarketplaceAddress = "0xc8C9c682Ac3cc6D9e329fAF60578040bDd145F3c";
+export const CryptoDevsDAOAddress = "0x0892BB389242F65DbC65BA62cDC8896C16d6f578";
+
+export const CryptoDevsNFTABI = []; // REPLACE THIS WITH THE NFT CONTRACT ABI
+export const FakeNFTMarketplaceABI = []; // REPLACE THIS WITH THE FAKE MARKETPLACE ABI
+export const CryptoDevsDAOABI = []; // REPLACE THIS WITH THE DAO ABI
